991e0571d1d47079cd0fc15c25e4fcdbaa3d96f6
Blaise1404 <Beliar1404@gmail.com>
2016-06-08 10:51:18 +0100

    07_06 plugins removed from the server

--- Source/GradGame/GradGameGameMode.cpp
+++ Source/GradGame/GradGameGameMode.cpp
@@ -1,6 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#pragma once
+#include "GradGame.h"
+#include "GradGameGameMode.h"
 
-#include "Engine.h"
+
+
 
---
+++ Source/GradGame/GradGameGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "GradGameGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GRADGAME_API AGradGameGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
